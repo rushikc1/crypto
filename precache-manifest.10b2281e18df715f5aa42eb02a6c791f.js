8b44ba219b35c7edf7c79d78d19496c92bb6e60f
rushikc <rushi743@gmail.com>
2021-05-10 22:36:17 +0530

    Updates

---
+++ precache-manifest.10b2281e18df715f5aa42eb02a6c791f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ace6b777151a2f8474e5ae0839ced5",
+    "url": "/crypto/index.html"
+  },
+  {
+    "revision": "c514e0216bc488481555",
+    "url": "/crypto/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "c514e0216bc488481555",
+    "url": "/crypto/static/js/2.cce81f24.chunk.js"
+  },
+  {
+    "revision": "8cc075ead67d321cfaced872e23d87fb",
+    "url": "/crypto/static/js/2.cce81f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d08b6f101d7034b5fa",
+    "url": "/crypto/static/js/main.d864ba85.chunk.js"
+  },
+  {
+    "revision": "b8a5f06207f95baa5370",
+    "url": "/crypto/static/js/runtime-main.fb1e2965.js"
+  }
+]);
